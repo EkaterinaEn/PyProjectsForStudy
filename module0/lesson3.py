6421876de6f8da1300c06db0ac3dbefda91b6436
u <u@ul.u>
2024-08-24 16:28:39 +0300

    String index and Variables

---
+++ module0/lesson3.py
@@ -0,0 +1,6 @@
+The_number_of_completed_DZ = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+print (Number_of_hours_spent/The_number_of_completed_DZ)
+Time_for_one_task = 0.125
+print('Курс:',Course_name,',всего задач:',The_number_of_completed_DZ,',затрачено часов:',Number_of_hours_spent,',среднее время выполнения:',Time_for_one_task, 'часа')
