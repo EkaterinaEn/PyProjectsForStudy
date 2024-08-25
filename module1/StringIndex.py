f64918de21a469b4fcbc66b9ecf0e110fa9b2416
u <u@ul.u>
2024-08-25 19:27:55 +0300

    fix StringIndex. вариант2

--- module1/StringIndex.py
+++ module1/StringIndex.py
@@ -1,6 +1,6 @@
 example = 'Ekaterina'
 print(example[0])
 print(example[-1])
-print(example[int(len (example)/2):len (example)])
+print(example[int(len (example)/2):])
 print(example[::-1])
 print(example[1::2])
