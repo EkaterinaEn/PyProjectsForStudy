6be4ea04fd550b5337209fd2a7f52bf12da8bc58
u <u@ul.u>
2024-09-05 10:38:23 +0300

    While

--- module2/module_2_3.py
+++ module2/module_2_3.py
@@ -3,6 +3,9 @@ a = 0
 while a < len(my_list):
     if my_list[a] > 0:
         print(my_list[a])
-    elif my_list[a] < 0:
+    elif my_list[a] == 0:
+        a = a + 1
+        continue
+    else:
         break
     a = a + 1
