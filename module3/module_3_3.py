6ee6a5add055f9ba11b426decc025bbbc055f25d
u <u@ul.u>
2024-09-10 19:15:41 +0300

    Unpacking positional parameters

---
+++ module3/module_3_3.py
@@ -0,0 +1,25 @@
+# 1.Функция с параметрами по умолчанию:
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+#2.Распаковка параметров:
+
+def print_params(a, b, c):
+    print(a, b, c)
+
+values_list = [1, 'список', False]
+values_dict = {'a': 2, 'b': 'словарь', 'c': 2}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+#3.Распаковка + отдельные параметры:
+def print_params(a, b, c):
+    print(a, b, c)
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
