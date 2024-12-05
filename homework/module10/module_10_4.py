0701c25e874960e4a54a69a77c5ebdb1e1f0bf46
u <u@ul.u>
2024-12-05 20:54:46 +0300

    Queues for data exchange between threads

--- homework/module10/module_10_4.py
+++ homework/module10/module_10_4.py
@@ -1,46 +1,84 @@
-from threading import Thread
+import threading
 from queue import Queue
 import time
 import random
 
-class Table(): #Table(1)
-    def __init__(self, number, guest):
+
+class Table:
+    def __init__(self, number):
         self.number = number
         self.guest = None
 
-class Guest(Thread): #Guest('Vasya')
+class Guest(threading.Thread):
     def __init__(self, name):
-        threading.Thread.__init__(self)
+        threading.Thread.__init__(self, name=name)
         self.name = name
 
     def run(self):
         time.sleep(random.randint(3,10))
 
-class Cafe(Table): #Cafe(Table(1), Table(2),....)
-    list_g = []
+class Cafe:
     def __init__(self, *tables):
+        self.tables = tables
         self.queue = Queue()
-        self.tables = list(tables)
-
-    def
 
     def guest_arrival(self, *guests):
-        g = list(guests)
-        t = self.tables
-        quantity_g = len(g)
-        free_tables = min(quantity_g, len(t))
-        for k in range(free_tables):
-            t[k].guest = guests[k]
-            thread1 = guests[k]
-            thread1.start()
-            Cafe.list_g.append(thread1)
-            print(f'{g[k].name} сел(-а) за стол номер {t[k].number}')
-        if quantity_g > free_tables:
-            for k in range(free_tables, quantity_g):
-                self.queue.put(guests[k])
-                print(f'{g[k].name} в очереди')
+        for guest in guests:
+            is_allocated = False
+            for table in tables:
+                if table.guest is None:
+                    table.guest = guest
+                    print(f"{guest.name} сел(-а) за стол номер {table.number}")
+                    is_allocated = True
+                    guest.start()
+                    break
+            if not is_allocated:
+                self.queue.put(guest)
+                print(f"{guest.name} в очереди")
 
     def discuss_guests(self):
+        while True:
+            free_table = 0
+            for table in tables:
+
+                if table.guest is not None and not table.guest.is_alive() :
+                    print(f"{table.guest.name} покушал(-а) и ушёл(ушла)")
+                    print(f"Стол номер {table.number} свободен")
+                    table.guest = None
+
+                if table.guest is None and not self.queue.empty():
+                    guest = self.queue.get()
+                    table.guest = guest
+                    guest.start()
+                    print(f'{guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}"')
+
+                if  table.guest is None:
+                    free_table += 1
+
+            if free_table >= len(tables):
+                break
+
+if __name__ == "__main__":
+
+    # Создание столов
+    tables = [Table(number) for number in range(1, 6)]
+
+    # Имена гостей
+    guests_names = [
+    'Maria', 'Oleg', 'Vakhtang', 'Sergey', 'Darya', 'Arman',
+    'Vitoria', 'Nikita', 'Galina', 'Pavel', 'Ilya', 'Alexandra']
+
+    # Создание гостей
+    new_guests = [Guest(name) for name in guests_names]
+
+    # Заполнение кафе столами
+    cafe = Cafe(*tables)
+
+    # Приём гостей
+    cafe.guest_arrival(*new_guests)
+
+    # Обслуживание гостей
+    cafe.discuss_guests()
 
 
 
