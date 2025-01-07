66edc8add1eca64b74937a0ae6ac35744ba984db
u <u@ul.u>
2025-01-07 20:42:55 +0300

    Asynchrony in practice

---
+++ homework/module13/module_13_1.py
@@ -0,0 +1,27 @@
+import time
+import asyncio
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования')
+    for k in range(5):
+        await asyncio.sleep(1/power)
+        print(f'Силач {name} поднял  {k + 1} шар')
+    print((f'Силач {name} закончил соревнования'))
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task1
+    await task2
+    await task3
+
+
+if __name__ == '__main__':
+    asyncio.run(start_tournament())
+
+
+
+
+
+
