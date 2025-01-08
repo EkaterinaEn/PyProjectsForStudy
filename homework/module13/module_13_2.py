4606853d08b25497268423b9ec9051d3f6a2e101
u <u@ul.u>
2025-01-08 15:49:04 +0300

    Message Handling Handlers

--- homework/module13/module_13_2.py
+++ homework/module13/module_13_2.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7942441570:AAHkzbfkMuUxZRyImHRocdwk5_2zPG7taM4"
+api = ""
 bot = Bot(token = api)
 dp = Dispatcher (bot, storage=MemoryStorage())
 
